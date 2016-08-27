b441229498947f898e39ee7c44417bd67d9b9c80
Yuki Maeda <my1866purin@gmail.com>
2016-08-27 17:05:48 +0900

    <play wav>

---
+++ wavplay.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+import wave
+import pyaudio
+
+#終了ができていない
+
+# 音をだす
+def play(wav, time):
+    
+    wf = wave.open(wav+".wav", "r")
+    # ストリーム開始
+    p = pyaudio.PyAudio()
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+
+    data = wf.readframes(1024)
+    t=0;
+    while(t<time):
+        stream.write(data)
+        data = wf.readframes(1024)
+        t=t+1
+    stream.close()      # ストリーム終了
+    p.terminate()
+
+
+if __name__ == '__main__':
+    play("test", 100)
+    
+    
+    
+    
+    
